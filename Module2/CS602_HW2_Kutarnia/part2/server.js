b15748ea50e4a23e30b60810c1b7c9fcbd6c1cb0
Eric Kutarnia <ekutarnia@mapfreusa.com>
2018-07-13 16:51:06 -0400

    Final working version before cleanup

--- Module2/CS602_HW2_Kutarnia/part2/server.js
+++ Module2/CS602_HW2_Kutarnia/part2/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+var js2xmlparser = require("js2xmlparser");
 
 // to parse request body
 const bodyParser = require("body-parser");
@@ -25,34 +26,89 @@ const employeeModule = require('./EmployeeModule.js');
 app.get('/', (req, res) => {
 	res.render('home');
 });
-// GET request for add employee
+// GET request for add employee view
 app.get('/addEmployee', (req, res) => {
 	res.render('newEmployee');
 });
-
-app.get('/about', (req, res) => {
-	res.render('about_quote',
-		{
-			quote: quotes.getRandomQuote(),
-			quotes: quotes.getAllQuotes()
-		});
-});
+//view all employees link
 app.get('/viewEmployees', (req, res) => {
 	res.render('employeeList', {
 		records: employeeModule.getAllEmployees()
 	});
-	/*
-		{quote: quotes.getRandomQuote(), 
-		 quotes: quotes.getAllQuotes()});
-		 */
+});
+//get employee by ID
+app.get('/viewEmployee/:id', (req, res) => {
+	res.format({
+		'application/json': () => {
+			res.json(cemployeeModule.lookupByID(req.params.id));
+		},
+
+		'application/xml': () => {
+			let record = js2xmlparser.parse("employee", employeeModule.lookupByID(req.params.id));
+			
+			res.type('application/xml');
+			res.send(record);
+		},
+
+		'text/html': () => {
+			res.render('employee', {
+				record: employeeModule.lookupByID(req.params.id)		
+			});
+		}
+	});
+});
+//get employee by ID satisfys the CURL HW step
+app.get('/id/:id', (req, res) => {
+	res.format({
+		'application/json': () => {
+			res.json(employeeModule.lookupByID(req.params.id));
+		},
+
+		'application/xml': () => {
+			let record = js2xmlparser.parse("employee", employeeModule.lookupByID(req.params.id));
+			
+			res.type('application/xml');
+			res.send(record);
+		},
+
+		'text/html': () => {
+			res.render('employee', {
+				record: employeeModule.lookupByID(req.params.id)		
+			});
+		}
+	});
+});
+
+//get by last name
+app.get('/lastName/:lastName', (req, res) => {
+	res.format({
+
+		'application/json': () => {
+			res.json(employeeModule.lookupByLastName(req.params.lastName));
+		},
+
+		'application/xml': () => {
+			let record = js2xmlparser.parse("employee", employeeModule.lookupByLastName(req.params.lastName));
+			
+			res.type('application/xml');
+			res.send(record);
+		},
+
+		'text/html': () => {
+			res.render('employeeList', {
+				records: employeeModule.lookupByLastName(req.params.lastName)
+			});
+		}
+	});
+
+	
 });
 
 //POST Requests
 app.post('/api/addEmployee', (req, res) => {
 	console.log("POST");
 	employeeModule.addEmployee(req.body.firstName, req.body.lastName);
-	res.redirect('/viewEmployees');
-	//res.json(courses.getAllCourses());
+	res.redirect('/lastName/'+'Smith');
 });
 
 app.use((req, res) => {
